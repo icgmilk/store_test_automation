26f1306bf3c086b53ad7c35132c6de8d11470754
kiritojim <jimasdfghjkl123456789@gmail.com>
2024-06-28 17:01:05 +0800

    Feat: 實現自動化測試功能
    - 實現登入網站、搜尋、點擊商品自動化測試(增加到購物車、登入尚有缺陷)
    - 新增readme文件

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # store_test_automation
+
+## 實作基本購物流程的自動化測試
+
+選擇網站: [普雷伊電視遊樂器專賣店](https://www.tvgame.com.tw/?lang=zh-TW)
+使用[Python](https://www.python.org/),[Pytest](https://docs.pytest.org/en/8.2.x/),[Selenium](https://www.selenium.dev/)
+## 使用方法
+
+
+
+clone下來後在test.py目錄下開啟powershell
+
+輸入
+```
+python -m pytest test.py -v -s
+```
+
--- test.py
+++ test.py
@@ -2,42 +2,63 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
 import time
-driver = webdriver.Chrome('driver/chromedriver.exe')
 
-#進入登入頁面
-url = 'https://www.tvgame.com.tw/V2/Login/Index/'
-driver.get(url)
-time.sleep(2)
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome('driver/chromedriver')
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
-#輸入手機號碼
-phone = driver.find_element(By.NAME, 'cellPhone')
-phone.send_keys('0937058761')
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div/form/div[2]/button').click()
-time.sleep(3)
+def test_enter(driver):
+    driver.get("https://www.tvgame.com.tw/")
+    assert driver.title == "普雷伊電視遊樂器專賣店"
 
-#輸入密碼
-password = driver.find_element(By.NAME, 'password')
-password.send_keys('test1101')
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div/form/button').click()
-time.sleep(8)
+def test_search(driver):
+    driver.get("https://www.tvgame.com.tw/")
+    search = driver.find_element(By.NAME, "q")
+    search.send_keys("switch")
+    search.send_keys(Keys.RETURN)
+    time.sleep(3)
+    assert "switch" in driver.title
 
-#搜尋商品
-goods_name = driver.find_element(By.NAME, 'q')
-goods_name.send_keys('瑪利歐賽車8 豪華版')
-time.sleep(1)
-goods_name.send_keys(Keys.ENTER)
-time.sleep(4)
+def test_click_commodity(driver):
+    driver.get("https://www.tvgame.com.tw/v2/Search?q=switch&shopId=2315")
+    click = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div/div/ul/li[1]/div/a/div/div[1]/div/figure/img')
+    click.click()
+    time.sleep(3)
+    assert "【NS】" in driver.title
 
-#點擊第一個商品
-first_goods = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div/div/ul/li[1]/div/a/div/div[1]/div/figure/img')
-first_goods.click()
-time.sleep(3)
+def test_add_to_cart(driver):
+    driver.get("https://www.tvgame.com.tw/SalePage/index/8912697")
+    add = driver.find_element(By.XPATH, '//*[@id="ns-add-to-cart"]/div/div[2]/div[2]/button[1]')
+    add.click()
+    time.sleep(3)
 
-#進到商品頁面點擊加入購物車
-shopping_cart = driver.find_element(By.XPATH, '//*[@id="ns-add-to-cart"]/div/div[2]/div[2]/button[1]')
-shopping_cart.click()
-time.sleep(3)
+    cart = driver.find_element(By.XPATH, '//*[@id="layout-header-fix"]/div/nav/ul/li[3]/a/span')
+    assert int(cart) == 1
+
+def test_login(driver):
+    # 進入登入頁面
+    url = 'https://www.tvgame.com.tw/V2/Login/Index/'
+    driver.get(url)
+    time.sleep(2)
+
+    # 輸入手機號碼
+    phone = driver.find_element(By.NAME, 'cellPhone')
+    phone.send_keys('0937xxxxxxxx')
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div/form/div[2]/button').click()
+    time.sleep(3)
+
+    # 輸入密碼
+    password = driver.find_element(By.NAME, 'password')
+    password.send_keys('testxxxxxxxxx')
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div/form/button').click()
+    time.sleep(8)
+
+    login_text = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/header/div[1]/div/div/nav/ul/li[2]/div/ul/li[5]/a').text
+    assert login_text == "會員登出", f"登出連結的文本應為 '會員登出'，但實際為 '{login_text}'"
