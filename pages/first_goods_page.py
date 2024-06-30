91e3a5517951a021cba23dff2dae1084c87bf656
kiritojim <jimasdfghjkl123456789@gmail.com>
2024-06-30 20:21:22 +0800

    fix: 修正test_v1.py異動

--- pages/first_goods_page.py
+++ pages/first_goods_page.py
@@ -12,8 +12,41 @@ class FirstGoodsPage(ActionUtils):
         super().__init__(driver)
         self.driver = driver
         self.add_to_cart_button_locator = (By.XPATH, '//span[text()="加入購物車"]')
-    
+        self.goods_title_locator = (By.XPATH, '//h1[@class = "salepage-title"]')
+        self.goods_price_locator = (By.XPATH, '//meta[@itemprop = "price"]')
+        self.goods_count_locator = (By.XPATH, '//input[@class = "qty-number-input ng-pristine ng-valid ng-not-empty ng-valid-min ng-valid-max ng-touched"]"]')
+        self.cart_button_locator = (By.XPATH, '//i[@class = "ico ico-shopping user-interaction-icon"]')
+
+    def get_goods_title(self):
+        """ Get the goods title
+        Returns:
+            str: The goods title
+        """
+        goods_title_element = self.find_visible_element(self.goods_title_locator)
+        return goods_title_element.text
+
+    def get_goods_price(self):
+        """ Get the goods price
+        Returns:
+            str: The goods price
+        """
+        goods_price_element = self.find_visible_element(self.goods_price_locator)
+        return goods_price_element.text
+
+    def get_goods_count(self):
+        """ Get the goods count
+        Returns:
+            str: The goods count
+        """
+        goods_count_element = self.find_visible_element(self.goods_count_locator)
+        return goods_count_element.get_attribute('value')
+
     def click_add_to_cart_button(self):
         """ Click the add to cart button """
         add_to_cart_button_element = self.find_clickable_element(self.add_to_cart_button_locator)
         add_to_cart_button_element.click()
+
+    def click_cart_button(self):
+        """ Click the cart button """
+        cart_button_element = self.find_clickable_element(self.cart_button_locator)
+        cart_button_element.click()
--- tests/test_v1.py
+++ tests/test_v1.py
@@ -15,46 +15,57 @@ from pages.home_page import HomePage
 from pages.login_page import LoginPage
 from pages.search_result_page import SearchResultPage
 from pages.first_goods_page import FirstGoodsPage
+from pages.cart_page import CartPage
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-options = Options()
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=1920,1080")  
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.maximize_window()  
 
 login_phone = os.getenv('PYTEST_LOGIN_PHONE')
 login_password = os.getenv('PYTEST_LOGIN_PASSWORD')
 
 def find_element(by, value):
     return (by, value)
+def test_login_then_add_product_to_cart():
+    #進入登入頁面
+    url_login = 'https://www.tvgame.com.tw/V2/Login/Index/'
+    driver.get(url_login)
 
-#進入登入頁面
-url = 'https://www.tvgame.com.tw/V2/Login/Index/'
-driver.get(url)
+    #輸入手機號碼
+    loginpage = LoginPage(driver)
+    loginpage.input_cell_phone(login_phone)
+    loginpage.click_login_register_button()
 
-#輸入手機號碼
-loginpage = LoginPage(driver)
-loginpage.input_cell_phone(login_phone)
-loginpage.click_login_register_button()
+    #輸入密碼
+    loginpage.input_password(login_password)
+    time.sleep(3)
+    loginpage.click_login_button()
 
-#輸入密碼
-loginpage.input_password(login_password)
-time.sleep(3)
-loginpage.click_login_button()
+    #搜尋商品
 
-#搜尋商品
+    homepage = HomePage(driver)
+    homepage.wait_login()
+    homepage.input_search_text('瑪利歐賽車8 豪華版')
+    homepage.click_search_button()
 
-homepage = HomePage(driver)
-homepage.wait_login()
-homepage.input_search_text('瑪利歐賽車8 豪華版')
-homepage.click_search_button()
+    #點擊第一個商品
+    search_result_page = SearchResultPage(driver)
+    search_result_page.click_first_goods()
 
-#點擊第一個商品
-search_result_page = SearchResultPage(driver)
-search_result_page.click_first_goods()
+    #進到商品頁面點擊加入購物車
+    first_goods_page = FirstGoodsPage(driver)
 
-#進到商品頁面點擊加入購物車
-first_goods_page = FirstGoodsPage(driver)
-first_goods_page.click_add_to_cart_button()
-time.sleep(3)
+    good_title = first_goods_page.get_goods_title()
+
+
+    first_goods_page.click_add_to_cart_button()
+    first_goods_page.click_cart_button()
+
+    cart_page = CartPage(driver)
+
+    assert good_title == cart_page.get_goods_title()
